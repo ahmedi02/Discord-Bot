4ce3274b5519ab7fab211e365f3ce61daa58bac9
Izhan Ahmed <140279715+ahmedi02@users.noreply.github.com>
2023-08-06 06:04:54 -0500

    Update responses.py

--- responses.py
+++ responses.py
@@ -1,11 +1,11 @@
-#Since we got bot.py to work and now the bot is online, now I am coming the bot to respond to users on my private (in-progress) discord server
+#Since we got bot.py to work and now the bot is online, now I am naking the bot to respond to users in my private (in-progress) discord server
 
 import discord
 import getting discord bot online first.py
 from discord.ext import commands
 
 @client.command()
-async def hello(welcome, paynent):
-    await welcome.send("Hello i am bot")
+async def hello(ctx):
+    await ctx.send("Hello I am a bot created by Izhan")
 
 #only got this one response working, I am currently working on how to get more than one response working 
